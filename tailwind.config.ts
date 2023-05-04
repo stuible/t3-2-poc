3f716aac7a1d4353d93881b155354842fd9700bf
Josh Stuible <joshstuible@gmail.com>
2023-05-04 15:36:00 -0700

    Play around with design system tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,25 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      // 'blue': '#1fb6ff',
+      // 'purple': '#7e5bef',
+      // 'pink': '#ff49db',
+      // 'orange': '#ff7849',
+      // 'green': '#13ce66',
+      // 'yellow': '#ffc82c',
+
+      'gray': {
+        light: '#d1d5db',
+        DEFAULT: '#6b7280',
+        dark: '#1f2937',
+      },
+
+    },
+    fontFamily: {
+      // sans: ['Graphik', 'sans-serif'],
+      // serif: ['Merriweather', 'serif'],
+    },
     extend: {},
   },
   plugins: [
