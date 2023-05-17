0d9c6c42942d790a357206cf7c2ec5c683abaa06
Josh Stuible <joshstuible@gmail.com>
2023-05-17 14:18:11 -0700

    Polish styling, render icon which can be passed in as prop

--- src/components/ui/ActionButton.tsx
+++ src/components/ui/ActionButton.tsx
@@ -2,13 +2,12 @@ import type { ReactNode } from "react";
 import Button from "./Button";
 
 const ActionButton = (
-    { title, href, target }: { title: string, href?: string | undefined, target?: string | undefined }
+    { title, href, target, icon }: { title: string, href?: string | undefined, target?: string | undefined, icon?: ReactNode }
 ) => {
     return (
-        <Button href={href} target={target} className="rounded-md bg-blue-lightest p-2 text-blue">
-            {/* <span>Icon</span>
-            <br /> */}
-            <span className="uppercase text-sm font-bold">{title}</span>
+        <Button href={href} target={target} className="rounded-md text-center bg-blue-lightest py-1 px-2 text-blue hover:bg-blue-lighter hover:text-white  active:bg-blue active:text-white">
+            {icon ? <span className="flex justify-center pt-1">{icon}</span> : false}
+            <span className="uppercase text-xs font-bold">{title}</span>
         </Button>
     )
 }
